0886f8be451e7a48f46bd65c8b8e3f1e59912cf1
sierraelery8 <sierra.elery@centenaryuniversity.edu>
2026-03-15 11:31:48 -0400

    setup.js

---
+++ database/setup.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const { Sequelize, DataTypes } = require('sequelize');
+
+// create a sequelize connection
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: process.env.DB_NAME
+});
+
+// define the track model
+const Track = sequelize.define('Track', {
+    trackId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    songTitle: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    artistName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    albumName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    duration: {
+        type: DataTypes.INTEGER
+    },
+    releaseYear: {
+        type: DataTypes.INTEGER
+    }
+});
+
+// async function to create a database and table
+async function setupDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log("Database connected.");
+
+        await sequelize.sync();
+        console.log("Tables created.");
+
+    } catch (error) {
+        console.error("Error setting up database:", error);
+    } finally {
+        await sequelize.close();
+    }
+}
+
+setupDatabase();
